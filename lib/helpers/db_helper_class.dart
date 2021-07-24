d83ae796274bf0f64f1ec2745528d0e1afd23497
Adaigwe <perculiaradaigwe1@gmail.com>
2021-07-24 09:50:48 +0100

    new update

--- lib/helpers/db_helper_class.dart
+++ lib/helpers/db_helper_class.dart
@@ -73,3 +73,20 @@ class DbHelperClass {
     await db.delete(tableName, where: "id=?", whereArgs: [id]);
   }
 }
+
+//Data Model sample to json to a model class
+// class DataModel {
+//   int? id;
+//   final String title;
+//   final String subtitle;
+//   DataModel({this.id, required this.title, required this.subtitle});
+
+//   factory DataModel.fromMap(Map<String, dynamic> json) => DataModel(
+//       id: json['id'], title: json["title"], subtitle: json["subtitle"]);
+
+//   Map<String, dynamic> toMap() => {
+//         "id": id,
+//         "title": title,
+//         "subtitle": subtitle,
+//       };
+// }
