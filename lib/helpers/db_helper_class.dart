5d23661e5ae82f973809a84ded18b12507ad0379
Adaigwe <perculiaradaigwe1@gmail.com>
2021-07-24 09:45:19 +0100

    update

--- lib/helpers/db_helper_class.dart
+++ lib/helpers/db_helper_class.dart
@@ -1,8 +1,10 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+//STILL WORKING ON THIS CLASS
+
 class DbHelperClass {
-  Future<Database> initDB(
+  static Future<Database> initDB(
       {String databaseName = "", String createTableQuery = ""}) async {
     //databaseName = "MYDb.db";
     // createTableQuery = """
@@ -22,29 +24,52 @@ class DbHelperClass {
     );
   }
 
-  Future<bool> insertData({
+  static Future<bool> insertData({
     dynamic dataModel,
-    String dataString = '',
+    required String tableName,
+    required String createTableQuery,
+    required String databaseName,
   }) async {
-    final Database db = await initDB();
-    db.insert("MYTable", dataModel.toMap());
+    final Database db = await initDB(
+        createTableQuery: createTableQuery, databaseName: databaseName);
+    db.insert(tableName, dataModel.toMap());
     return true;
   }
 
-  Future<List<dynamic>> getData(dynamic dataModel) async {
-    final Database db = await initDB();
-    final List<Map<String, Object?>> datas = await db.query("MYTable");
+  Future<List<dynamic>> getData({
+    dynamic dataModel,
+    required String tableName,
+    required String createTableQuery,
+    required String databaseName,
+  }) async {
+    final Database db = await initDB(
+        createTableQuery: createTableQuery, databaseName: databaseName);
+    final List<Map<String, Object?>> datas = await db.query(tableName);
     return datas.map((e) => dataModel.fromMap(e)).toList();
   }
 
-  Future<void> update(dynamic dataModel, int id) async {
-    final Database db = await initDB();
+  Future<void> update({
+    dynamic dataModel,
+    required int id,
+    required String tableName,
+    required String createTableQuery,
+    required String databaseName,
+  }) async {
+    final Database db = await initDB(
+        createTableQuery: createTableQuery, databaseName: databaseName);
     await db
-        .update("MYTable", dataModel.toMap(), where: "id=?", whereArgs: [id]);
+        .update(tableName, dataModel.toMap(), where: "id=?", whereArgs: [id]);
   }
 
-  Future<void> delete(int id) async {
-    final Database db = await initDB();
-    await db.delete("MYTable", where: "id=?", whereArgs: [id]);
+  Future<void> delete({
+    required int id,
+    dynamic dataModel,
+    required String tableName,
+    required String createTableQuery,
+    required String databaseName,
+  }) async {
+    final Database db = await initDB(
+        createTableQuery: createTableQuery, databaseName: databaseName);
+    await db.delete(tableName, where: "id=?", whereArgs: [id]);
   }
 }
